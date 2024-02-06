c951008dce02499fab0f5c0e9d28344932fb1f8e
ValLilian <lilian.vallienne@gmail.com>
2024-02-06 15:47:05 +0100

    Fix login

--- script/getLogin.js
+++ script/getLogin.js
@@ -1,6 +1,4 @@
 import {ip} from '../conf.js';
-// import axios from "axios";
-// const axios = axios
 
 export async function getLogin(username, password) {
     let response;
@@ -20,11 +18,9 @@ export async function getLogin(username, password) {
 
         // Extrait les données de la réponse
         const responseData = await response.json();
-        alert('Requête effectuée avec succès !');
         return responseData.token;
     } catch (error) {
         console.error(error);
-        alert('Une erreur s\'est produite lors de la requête.');
         return null;
     }
 }
