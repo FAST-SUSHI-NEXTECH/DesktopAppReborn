130c89747348100aa51dfb84e7974a310c14b9a0
ValLilian <lilian.vallienne@gmail.com>
2024-02-14 10:26:52 +0100

    Add order + ordercontent & fix orders

---
+++ script/getOrderByID.js
@@ -0,0 +1,25 @@
+import {ip} from '../conf.js';
+
+export async function getOrderByID(id_order) {
+    try {
+        const response = await fetch(ip + '/order/id', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + sessionStorage.getItem('token')
+            },
+            body: JSON.stringify({id_order})
+        });
+
+        if (!response.ok) {
+            throw new Error('La requête a échoué');
+        }
+
+        const data = await response.json();
+        return (data);
+    } catch (error) {
+        alert(error);
+        console.log(error);
+        return null; // Renvoie null en cas d'erreur
+    }
+}
